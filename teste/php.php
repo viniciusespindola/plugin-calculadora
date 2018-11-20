83e659b7217b33341c3be3183b027cbde41be48b
Vinicius Marinho <vm_espindola@hotmail.com>
2018-11-20 16:02:33 +0000

    Pasta com o plugin da calculadora

---
+++ teste/php.php
@@ -0,0 +1,8 @@
+<?php>
+            $primeiro = $_POST['primerio-número'];
+            
+            $segundo = $_POST['segundo-número'];
+            
+            $resultado = $primerio * $segundo;
+            return $resultado;
+        ?>
