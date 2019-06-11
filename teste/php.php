859c1ffcca06aef13c3d825eda243f7d1cf8f873
viniciusespindola <vinicius.espindola@etec.sp.gov.br>
2019-06-11 01:35:52 -0300

    Retirada de arquivos desnecessários

--- teste/php.php
+++
@@ -1,8 +0,0 @@
-<?php>
-            $primeiro = $_POST['primerio-número'];
-            
-            $segundo = $_POST['segundo-número'];
-            
-            $resultado = $primerio * $segundo;
-            return $resultado;
-        ?>
